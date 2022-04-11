533297b4978bcc93b1e46f3c1aa858da5955b8b1
lirp01 <101674256+lirp01@users.noreply.github.com>
2022-04-11 15:11:19 -0400

    Add files via upload

---
+++ lab1_u2/ejercicio3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+    define('TAM',10);
+    echo "<table border=1>"; 
+    $contador=1;
+    for ($n=1; $n<=TAM; $n++){
+        if ($n % 2 == 0)
+        echo "<tr bgcolor=#bdc3d6>";
+        else
+        echo "<tr>"; 
+        for ($j=1; $j<=TAM; $j++){
+            echo "<td>", $contador, "</td>";
+            $contador=$contador+1;
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+?>
+</body>
+</html>
